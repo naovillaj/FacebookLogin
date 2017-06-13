8dbf907d60d067710e211a96a43ed5e1e4110836
Naomi Villanueva <naovillaj@gmail.com>
2017-06-13 16:59:19 -0500

    Inicio sesión con API FB

---
+++ js/utils/facebook.js
@@ -0,0 +1,27 @@
+window.fbAsyncInit = () => {
+	FB.init({
+		appId : "1920730988250531",
+		cookie : true,
+		xfbml : true,
+		version : "v2.8"
+	});
+};
+
+
+function loginHandler(response) {
+	if(response.status === "connected") {
+		state.status = "Conectado";
+		FB.api("/me?fields=email,name", user => {
+			state.user = user;
+			state.doRender();
+		});
+	} else if (response.status === "not_authorized") {
+		state.user = null;
+		state.status = "Aplicación no autorizada";
+		state.doRender();
+	}
+}
+
+function doLogin() {
+	FB.login(loginHandler, {scope: "email"});
+}
